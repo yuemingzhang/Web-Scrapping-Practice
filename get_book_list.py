a2e9c97d3ae3cade5e2ec7981307f636bd2c2229
Yueming Zhang <yueming.zhang0704@gmail.com>
2019-03-21 23:15:43 -0700

    Update get_book_list.py
    Add code to get the book titles, author names and book links for the top 100 books.

--- get_book_list.py
+++ get_book_list.py
@@ -1,7 +1,40 @@
-import requests
-import os
-# extract the book name and corresponding url from the Best_Books_Ever_Goodreads.html
-url = 'https://www.goodreads.com/list/show/1.Best_Books_Ever'
-response = requests.get(url)
-with open(os.path.join(url.split('/')[-1]), mode='wb') as file:
-        file.write(response.content)
+# get html page
+import urllib.request
+url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
+page = urllib.request.urlopen(url)
+
+# extract names from the html page
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(page, 'html.parser')
+soup.find_all('span',itemprop="name")
+
+# check the length, confirm there are 100 book titles with 100 author names
+len(soup.find_all('span',itemprop="name"))
+
+# save the book titles and authors in the dataframe
+import pandas as pd
+book_list = []
+for i in range(0, len(soup.find_all('span',itemprop="name")), 2):
+    book_title = soup.find_all('span',itemprop="name")[i].string
+    author = soup.find_all('span',itemprop="name")[i+1].string
+    book_list.append({'book_title': book_title,
+                      'author': author})
+df = pd.DataFrame(book_list, columns = ['book_title', 'author'])
+df
+
+# extract urls from the html page
+links = []
+for i in soup.find_all(itemprop="url"):
+    link = i.get("href")
+    links.append(link)
+links
+
+# check the number of urls, confirm there are 100 book short url and 100 author links
+len(links)
+
+# save book links in the dataframe
+book_links = []
+for i in range(0, len(links), 2):
+    book_links.append('https://www.goodreads.com'+links[i])
+    df['book_link'] = pd.DataFrame(book_links)
+df
