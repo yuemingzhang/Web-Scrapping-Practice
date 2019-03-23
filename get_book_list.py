b4ea4fdde4771de0c2cd5c59531f2e5054fe3ec9
Yueming Zhang <yueming.zhang0704@gmail.com>
2019-03-22 23:32:50 -0700

    Update get_book_list.py
    test the first book html page to extract star ratings, rating count and review count.

--- get_book_list.py
+++ get_book_list.py
@@ -38,3 +38,35 @@ for i in range(0, len(links), 2):
     book_links.append('https://www.goodreads.com'+links[i])
     df['book_link'] = pd.DataFrame(book_links)
 df
+
+# Open each book's link to get the book information
+for book_link in df['book_link']:
+    link = urllib.request.urlopen(book_link)
+    soup = BeautifulSoup(link, 'html.parser')
+    soup.find_all('span', itemprop="name")
+    # test below
+
+    # first book's link
+    df['book_link'][0]
+    
+    # get star ratings
+    import re
+    link = urllib.request.urlopen(df['book_link'][0])
+    booksoup = BeautifulSoup(link, 'html.parser')
+    rating_details = booksoup.find_all('script', type="text/javascript+protovis")[0].string
+    five_star = int(re.findall(r'\d+', rating_details)[0])
+    four_star = int(re.findall(r'\d+', rating_details)[1])
+    three_star = int(re.findall(r'\d+', rating_details)[2])
+    two_star = int(re.findall(r'\d+', rating_details)[3])
+    one_star = int(re.findall(r'\d+', rating_details)[4])
+    
+    # get rating count
+    rating_count = int(booksoup.find_all('meta', itemprop="ratingCount")[0]['content'])
+    rating_count
+    
+    # get review count
+    review_count = int(booksoup.find_all('meta', itemprop="reviewCount")[0]['content'])
+    review_count
+
+
+    
